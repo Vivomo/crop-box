b03b61b004c2b33981ca91bf6b53e6c0c5b52a39
Vimo <929992114@qq.com>
2019-01-19 19:44:13 +0800

    build

---
+++ src/htmlTemplate.js
@@ -0,0 +1,13 @@
+const html = `
+  <div class="cropper">
+            <div class="square s-lt" data-left="1" data-top="1"></div>
+            <div class="square s-t" data-top="1"></div>
+            <div class="square s-rt" data-width="1" data-top="1"></div>
+            <div class="square s-l" data-left="1"></div>
+            <div class="square s-r" data-width="1"></div>
+            <div class="square s-lb" data-left="1" data-height="1"></div>
+            <div class="square s-rb" data-width="1" data-height="1"></div>
+            <div class="square s-b" data-height="1"></div>
+        </div>
+`
+export default html;
